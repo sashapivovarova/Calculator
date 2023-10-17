fc7fb2ddffe8b5beff1c1b1979a716c7c96d3967
sashapivovarova <78265489+sashapivovarova@users.noreply.github.com>
2023-10-17 11:51:12 +0900

    Initial Commit

---
+++ Calculator/CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorApp.swift
+//  Calculator
+//
+//  Created by Anastasia Grey on 2023/10/17.
+//
+
+import SwiftUI
+
+@main
+struct CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
